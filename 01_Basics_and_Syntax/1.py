36ff80bf6af6c9e67ac69a99916af17896ac85d0
seraphicbaby <Tanisha17016@gmail.com>
2026-03-14 13:22:35 +0530

    day 1

---
+++ 01_Basics_and_Syntax/1.py
@@ -0,0 +1,5 @@
+# simple prog:Take and print avg of two variables
+mon = int(input("enter num 1: "))      # NOTE: int comes outside and then input comes inside it
+tue = int(input("enter num 2: "))
+avg = (mon+tue)/2
+print("the avg of",mon," and ",tue," is ",avg)
---
+++ 01_Basics_and_Syntax/2.py
@@ -0,0 +1,8 @@
+# how  to take input in different types
+# take age and height inputs from the user in different data types and then print them with type
+# python takes input in str by default
+
+age = int(input("enter age: "))
+height = float(input("enter height: "))
+print(age, type(age))
+print(height,type(height))
